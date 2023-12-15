318c77c8f04b39b5ea89970a968085c3bd595403
shashank26-cmd <shashankbud80@gmail.com>
2023-12-16 00:49:02 +0530

    third commit

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -4,76 +4,90 @@ import { useState, useEffect } from "react";
 import Gallery from "../Galllery/Gallery";
 
 function GalleryList() {
-  const [loading, setloading] = useState(true);
-  const [List, setList] = useState([]);
-  const [offset, setOffset] = useState(0);
+
+  const [Advance, setAdvance] = useState({
+    loading: true,
+    List: [],
+    offset: 0
+  });
 
   const baseUrl = "https://api.slingacademy.com/v1/sample-data/photos";
 
-  async function downloadList() {
+  const downloadList = async () => {
     try {
-      const response = await axios.get(`${baseUrl}?offset=${offset}&limit=10`);
+      const response = await axios.get(`${baseUrl}?offset=${Advance.offset}&limit=10`);
       console.log(response.data);
 
-      const promise=response.data.photos
-      console.log(promise)
+      const promise = response.data.photos;
+      console.log(promise);
 
       const galleryPhotos = promise.map((p) => ({
         key: p.id,
         name: p.title,
         img: p.url,
         desc: p.description,
-        id:p.id
+        id: p.id
       }));
 
-      setList(galleryPhotos);
-      setloading(false);
+      setAdvance((prevState) => ({
+        ...prevState,
+        List: galleryPhotos,
+        loading: false
+      }));
     } catch (error) {
       console.error("Error fetching data:", error);
-      setloading(false);
+      setAdvance((prevState) => ({
+        ...prevState,
+        loading: false
+      }));
     }
-  }
+  };
 
   useEffect(() => {
     downloadList();
-  }, [offset]);
+  }, [Advance.offset]);
 
   const handleNext = () => {
-    if (List.length === 10) {
+    if (Advance.List.length === 10) {
       // Assuming limit is always 10
-      setOffset(offset + 10);
+      setAdvance(prevState => ({
+        ...prevState,
+        offset: prevState.offset + 10
+      }));
     }
   };
 
   const handlePrev = () => {
-    if (offset >= 10) {
+    if (Advance.offset >= 10) {
       // Assuming limit is always 10
-      setOffset(Math.max(0, offset - 10));
+      setAdvance(prevState => ({
+        ...prevState,
+        offset: Math.max(0, prevState.offset - 10)
+      }));
     }
   };
 
   return (
-    
     <div>
       <div className="flex flex-wrap justify-center">
-        {loading ? (
+        {Advance.loading ? (
           'is loading..'
         ) : (
-          List.map((p) => <Gallery name={p.name} img={p.img} key={p.key}  id={p.id}/>)
+          Advance.List.map((p) => <Gallery name={p.name} img={p.img} key={p.key} id={p.id} />)
         )}
       </div>
       <div className="flex justify-center mt-4">
         <button
           className="bg-blue-500 text-white px-4 py-2 mr-2"
           onClick={handlePrev}
-          disabled={offset < 10}
+          disabled={Advance.offset < 10}
         >
           Previous
         </button>
         <button
           className="bg-blue-500 text-white px-4 py-2"
           onClick={handleNext}
-          disabled={List.length !== 10}
+          disabled={Advance.List.length !== 10}
         >
           Next
         </button>
